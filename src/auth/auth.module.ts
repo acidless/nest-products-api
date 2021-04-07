00149995f609e779afac1218e6acd194a90d9a98
Stasrikan <rubtid228@gmail.com>
2021-04-07 21:55:42 +0300

    Authentication separated from users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JWTService } from './JWT/JWT.service';
+import { AuthController } from './auth.controller';
 
 /*====================*/
 
 @Module({
+  controllers: [AuthController],
   imports: [UserModule],
   providers: [AuthService, JWTService],
 })
